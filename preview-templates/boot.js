e57cb6d57e912412e2379e7c858beb73d966df42
knox <knox.h@qq.com>
2020-05-07 16:31:39 +0800

    Adapter 2.4.0

--- preview-templates/boot.js
+++ preview-templates/boot.js
@@ -3,19 +3,19 @@
 
     var canvas = document.getElementById('GameCanvas');
     window.onload = function () {
-        if (window.__quick_compile__) {
-            window.__quick_compile__.load(onload);
+        if (window.__quick_compile_engine__) {
+            window.__quick_compile_engine__.load(onload);
         }
         else {
             onload();
         }
     };
-    
+
     function onload () {
 
         // socket
         // =======================
-        
+
         // Receives a refresh event from the editor, which triggers the reload of the page
         var socket = window.io();
         socket.on('browser:reload', function () {
@@ -31,40 +31,12 @@
         // init engine
         // =======================
 
-        var AssetOptions = {
-            libraryPath: 'res/import',
-            rawAssetsBase: 'res/raw-',
-            rawAssets: _CCSettings.rawAssets
-        };
+        var engineInited = false;
 
-        // jsList
-        var jsList = _CCSettings.jsList || [];
-        jsList = jsList.map(function (x) { return AssetOptions.rawAssetsBase + x; });
-        if (_CCSettings.jsBundleForWebPreview) {
-            jsList.push(_CCSettings.jsBundleForWebPreview);
-        }
-
-        window.__modular.init(_CCSettings.scripts);
-        jsList = jsList.concat(window.__modular.srcs);
-
-        var option = {
-            id: canvas,
-            scenes: _CCSettings.scenes,
-            debugMode: _CCSettings.debug ? cc.debug.DebugMode.INFO : cc.debug.DebugMode.ERROR,
-            showFPS: _CCSettings.debug,
-            frameRate: 60,
-            groupList: _CCSettings.groupList,
-            collisionMatrix: _CCSettings.collisionMatrix,
-            jsList: jsList
-        };
-
-        cc.AssetLibrary.init(AssetOptions);
-
-        cc.game.run(option, function () {
-
+        var onStart = function () {
             cc.view.enableRetina(true);
-            cc.debug.setDisplayStats(true);
-        
+            cc.view.resizeWithBrowserSize(true);
+
             // Loading splash scene
             var splash = document.getElementById('splash');
             var progressBar = splash.querySelector('.progress-bar span');
@@ -75,42 +47,65 @@
 
             cc.game.pause();
 
-            // load stashed scene
-            cc.loader.load('preview-scene.json', function (error, json) {
-                if (error) {
-                    cc.error(error.stack);
+            // init assets
+            engineInited = true;
+
+            cc.assetManager.loadAny({url: 'preview-scene.json', __isNative__: false }, null, function (finish, totalCount) {
+                var percent = 100 * finish / totalCount;
+                if (progressBar) {
+                    progressBar.style.width = percent.toFixed(2) + '%';
+                }
+            }, function (err, sceneAsset) {
+                if (err) {
+                    console.error(err.message, err.stack);
                     return;
                 }
-
-                cc.loader.onProgress = function (completedCount, totalCount, item) {
-                    var percent = 100 * completedCount / totalCount;
-                    if (progressBar) {
-                        progressBar.style.width = percent.toFixed(2) + '%';
-                    }
-                };
-
-                cc.AssetLibrary.loadJson(json,
-                    function (err, sceneAsset) {
-                        if (err) {
-                            cc.error(err.stack);
-                            return;
-                        }
-                        var scene = sceneAsset.scene;
-                        scene._name = sceneAsset._name;
-                        cc.director.runSceneImmediate(scene, function () {
-                            // play game
-                            cc.game.resume();
-                        });
-
-                        cc.loader.onProgress = null;
-                    }
-                );
+                var scene = sceneAsset.scene;
+                scene._name = sceneAsset._name;
+                cc.director.runSceneImmediate(scene, function () {
+                    // play game
+                    cc.game.resume();
+                });
             });
 
             // purge
             //noinspection JSUnresolvedVariable
             _CCSettings = undefined;
+        };
+
+        var option = {
+            id: canvas,
+            debugMode: _CCSettings.debug ? cc.debug.DebugMode.INFO : cc.debug.DebugMode.ERROR,
+            showFPS: _CCSettings.debug,
+            frameRate: 60,
+            groupList: _CCSettings.groupList,
+            collisionMatrix: _CCSettings.collisionMatrix,
+        };
+
+        cc.assetManager.init({
+            importBase: 'assets/others/import',
+            nativeBase: 'assets/others/native'
         });
-    };
+
+        let { RESOURCES, INTERNAL, MAIN } = cc.AssetManager.BuiltinBundleName;
+        let bundleRoot = [RESOURCES, INTERNAL, MAIN];
+
+        var count = 0;
+        function cb (err) {
+            if (err) return console.error(err);
+            count++;
+            if (count === bundleRoot.length + 1) {
+                cc.game.run(option, onStart);
+            }
+        }
+
+        // load plugins
+        cc.assetManager.loadScript(_CCSettings.jsList.map(function (x) { return '/plugins/' + x; }), cb);
+
+        // load bundles
+        for (let i = 0; i < bundleRoot.length; i++) {
+            cc.assetManager.loadBundle('assets/' + bundleRoot[i], cb);
+        }
+    }
 })();
 
